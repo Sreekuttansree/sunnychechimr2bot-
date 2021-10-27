30009edb699f00eeead884d694ed52f948e7ca03
Sreekuttansree <88297456+Sreekuttansree@users.noreply.github.com>
2021-10-27 19:30:40 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -149,7 +149,7 @@ async def auto_filter(bot: Client, update: Message):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("𝐍𝐞𝐱𝐭  ⏩
+                    InlineKeyboardButton(" 𝐍𝐞𝐱𝐭  ⏩
 ⏭️", callback_data=f"navigate(0|next|{query})")
                 ]
             )
